9950bca20f99688f49855d45c3fe35241882956a
swatantragoswami09 <swatantra.goswami_cs17@gla.ac.in>
2022-11-28 22:27:10 +0530

    latest change

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import GameBoard from "./GameBoard";
 
 function App() {
-  console.log("hi there");
+  console.log("hi there 2");
   return (
     <div className="app-container">
       <GameBoard />
